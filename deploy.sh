ad189886e4af4f60b2fd924077fbf91596019667
Lucas Oliveira <loslucassilva@gmail.com>
2016-06-17 00:05:34 -0300

    Add commit and push to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,18 @@ echo "Deploying to the github page"
 
 cp -r ./public ../
 git checkout gh-pages
-rm -rf ./*
+
+rm -rf ./*.html
+rm -rf ./*.html
+rm -rf ./*.css
+rm -rf ./*.js
+rm -rf ./*.map
+
 cp -r ../public/* .
 rm -rf ../public
+
+git add .
+git commit -m "Deploying...."
+git push origin gh-pages
 
 echo 'Done!'
